5cbe105a9c43104e9b678b2a2d310f912f56071d
s.romanov <s.romanov@studiofact.ru>
2021-05-17 07:10:19 +0300

    test funcs

--- www/main.go
+++ www/main.go
@@ -1,7 +1,44 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
+
+type User struct {
+	name                  string
+	age                   uint16
+	money                 int16
+	avg_grades, happiness float64
+}
+
+func (u User) getAllInfo() string {
+	return fmt.Sprintf("User name is: %s. He is %d years old and he has money"+
+		" equal: %d", u.name, u.age, u.money)
+}
+
+func (u *User) setNewName(newName string) {
+	u.name = newName
+}
+
+func home_page(w http.ResponseWriter, r *http.Request) {
+	bob := User{name: "Bob", age: 25, money: -50, avg_grades: 4.2, happiness: 0.8}
+	bob.setNewName("Alex")
+	fmt.Fprintf(w, bob.getAllInfo())
+}
+
+func contacts_page(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Contacts page!")
+}
+
+func handleRequest() {
+	http.HandleFunc("/", home_page)
+	http.HandleFunc("/contacts/", contacts_page)
+	http.ListenAndServe(":8080", nil)
+
+}
 
 func main() {
-	fmt.Println("string")
+	//bob := User{name: "Bob", age: 25, money: -50, avg_grades: 4.2, happiness: 0.8}
+	handleRequest()
 }
